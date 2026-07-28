39567c216c80927af48eda7352db7678c6c02437
Ellen Habash <48154642+ellenhabash2@users.noreply.github.com>
2026-07-28 21:56:12 +0000

    feat(api): add flight operation endpoints

--- backend/controllers/flight_controller.h
+++ backend/controllers/flight_controller.h
@@ -1,7 +1,6 @@
-﻿#pragma once
+#pragma once
 #include <drogon/HttpController.h>
 #include <string>
-#include <memory>
 
 using namespace drogon;
 
@@ -11,10 +10,18 @@ public:
     METHOD_LIST_BEGIN
     ADD_METHOD_TO(FlightController::getFlights, "/flights", Get);
     ADD_METHOD_TO(FlightController::getDelayedFlights, "/flights/delayed", Get);
+    ADD_METHOD_TO(FlightController::searchFlights, "/flights/search", Get);
+    ADD_METHOD_TO(FlightController::getFlightByNumber, "/flights/number/{1}", Get);
+    ADD_METHOD_TO(FlightController::updateFlightStatus, "/flights/{1}/status", Patch, "JwtAuthFilter");
+    ADD_METHOD_TO(FlightController::assignFlightGate, "/flights/{1}/gate", Patch, "JwtAuthFilter");
     ADD_METHOD_TO(FlightController::getFlightById, "/flights/{1}", Get);
     METHOD_LIST_END
 
-    void getFlights(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
-    void getDelayedFlights(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback);
-    void getFlightById(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback, std::string id);
+    void getFlights(const HttpRequestPtr &, std::function<void(const HttpResponsePtr &)> &&);
+    void getDelayedFlights(const HttpRequestPtr &, std::function<void(const HttpResponsePtr &)> &&);
+    void searchFlights(const HttpRequestPtr &, std::function<void(const HttpResponsePtr &)> &&);
+    void getFlightByNumber(const HttpRequestPtr &, std::function<void(const HttpResponsePtr &)> &&, std::string flightNumber);
+    void getFlightById(const HttpRequestPtr &, std::function<void(const HttpResponsePtr &)> &&, std::string id);
+    void updateFlightStatus(const HttpRequestPtr &, std::function<void(const HttpResponsePtr &)> &&, std::string id);
+    void assignFlightGate(const HttpRequestPtr &, std::function<void(const HttpResponsePtr &)> &&, std::string id);
 };
